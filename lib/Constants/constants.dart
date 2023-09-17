1a4fa31653387d76fad1adc151798af70ed2c568
Mohammed Ashiq <mohammedashiq836@gmail.com>
2023-09-17 12:07:26 +0530

    Added 3 tabs to the appbar

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -1,9 +1,11 @@
-
 import 'package:flutter/material.dart';
 
 class Constants {
   static const String appname = "WhatsApp";
   static const Color whatsAppGreen = Color.fromRGBO(10, 83, 75, 0.886);
   static const Color whiteColor = Colors.white;
- 
+
+  // Tabs
+
+  static const List tabs = ["Chats", "Status", "Calls"];
 }
--- lib/Screen/home_page.dart
+++ lib/Screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_ui/Constants/constants.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,33 +7,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          const SliverAppBar(
-            backgroundColor: Constants.whatsAppGreen,
-            foregroundColor: Constants.whiteColor,
-            floating: true,
-            pinned: true,
-            snap: true,
-            expandedHeight: 90,
-            title: Text(
-              Constants.appname,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
+    return DefaultTabController(
+      length: Constants.tabs.length,
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              backgroundColor: Constants.whatsAppGreen,
+              foregroundColor: Constants.whiteColor,
+              floating: true,
+              pinned: true,
+              snap: true,
+              expandedHeight: 95.h,
+              title: const Text(
+                Constants.appname,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              bottom: TabBar(
+                indicatorColor: Constants.whiteColor,
+                indicatorSize: TabBarIndicatorSize.tab,
+                tabs: List.generate(
+                  Constants.tabs.length,
+                  (index) => _tab(
+                    Constants.tabs[index],
+                  ),
+                ),
               ),
             ),
-            
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) => Container(
-                
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) => Container(),
+                childCount: 50,
               ),
-              childCount: 50,
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+
+Padding  _tab(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Constants.whiteColor,
+          fontSize: 14.5.sp,
+          fontWeight: FontWeight.w500,
+        ),
       ),
     );
   }
