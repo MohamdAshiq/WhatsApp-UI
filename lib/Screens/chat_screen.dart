276cf35ca7670ff7681d752103a2203dfbdad416
Mohammed Ashiq <mohammedashiq836@gmail.com>
2023-09-29 15:29:26 +0530

    Added list of chats into Chats Screen

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -5,9 +5,36 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Chats Screen"),
+    return Scaffold(
+      body: ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: 15,
+        itemBuilder: (context, index) => ListTile(
+        
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 3,
+            horizontal: 5,
+          ),
+          onTap: () {},
+          horizontalTitleGap: 16,
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey[400],
+            foregroundColor: Colors.white,
+            radius: 25,
+            child: const Icon(
+              Icons.person,
+              size: 27,
+            ),
+          ),
+          title: Text(
+            "Chat ${index + 1}",
+            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+          ),
+          subtitle: Text(
+            "Description ${index + 1}",
+            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+          ),
+        ),
       ),
     );
   }
