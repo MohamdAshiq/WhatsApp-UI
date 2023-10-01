d7bb4a33ce44cf50896dba88e442fadeccd9908e
Mohammed Ashiq <mohammedashiq836@gmail.com>
2023-10-01 17:44:46 +0530

    Refactored the code of Calls Screen

--- lib/Screens/Calls Screen/calls_screen.dart
+++ lib/Screens/Calls Screen/calls_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_ui/Constants/constants.dart';
+import 'package:whatsapp_ui/Screens/Calls%20Screen/recent_call_section.dart';
 import 'package:whatsapp_ui/Widgets/floating_action_button_widget.dart';
 
 class CallsScreen extends StatelessWidget {
@@ -44,79 +45,13 @@ class CallsScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 10,
-                ),
-                child: Text(
-                  "Recent",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16.sp,
-                  ),
-                ),
-              ),
-              Column(
-                children: List.generate(
-                  10,
-                  (index) => ListTile(
-                    onTap: () {},
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.grey[400],
-                      foregroundColor: Colors.white,
-                      radius: 25.r,
-                      child: const Icon(
-                        Icons.person,
-                        size: 30,
-                      ),
-                    ),
-                    title: Text(
-                      "Chat Call ${index + 1}",
-                      style: TextStyle(
-                          fontSize: 14.5.sp,
-                          fontWeight: FontWeight.w600,
-                          color: index.isOdd ? Colors.black : Colors.redAccent),
-                    ),
-                    subtitle: Row(
-                      children: [
-                        index.isEven
-                            ? const Icon(
-                                Icons.arrow_outward,
-                                size: 18,
-                                color: Colors.green,
-                              )
-                            : const Icon(
-                              Icons.call_received,
-                              size: 18,
-                              color: Colors.red,
-                            ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "September ${index+1}, 7:0$index PM",
-                          style: TextStyle(
-                              fontSize: 12.5.sp,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.grey[700]),
-                        ),
-                      ],
-                    ),
-                    trailing: const Icon(
-                      Icons.call,
-                      color: Color.fromARGB(255, 74, 161, 79),
-                    ),
-                  ),
-                ),
-              )
+              const RecentCallSection()
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButtonWidget(onPressed: () {
-        
-      }, icon: Icons.add_ic_call_sharp),
+      floatingActionButton: FloatingActionButtonWidget(
+          onPressed: () {}, icon: Icons.add_ic_call_sharp),
     );
   }
 }
---
+++ lib/Screens/Calls Screen/recent_call_section.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RecentCallSection extends StatelessWidget {
+  const RecentCallSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 10,
+          ),
+          child: Text(
+            "Recent",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16.sp,
+            ),
+          ),
+        ),
+        Column(
+          children: List.generate(
+            10,
+            (index) => ListTile(
+              onTap: () {},
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey[400],
+                foregroundColor: Colors.white,
+                radius: 25.r,
+                child: const Icon(
+                  Icons.person,
+                  size: 30,
+                ),
+              ),
+              title: Text(
+                "Chat Call ${index + 1}",
+                style: TextStyle(
+                    fontSize: 14.5.sp,
+                    fontWeight: FontWeight.w600,
+                    color: index.isOdd ? Colors.black : Colors.redAccent),
+              ),
+              subtitle: Row(
+                children: [
+                  index.isEven
+                      ? const Icon(
+                          Icons.arrow_outward,
+                          size: 18,
+                          color: Colors.green,
+                        )
+                      : const Icon(
+                          Icons.call_received,
+                          size: 18,
+                          color: Colors.red,
+                        ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "September ${index + 1}, 7:0$index PM",
+                    style: TextStyle(
+                        fontSize: 12.5.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey[700]),
+                  ),
+                ],
+              ),
+              trailing: const Icon(
+                Icons.call,
+                color: Color.fromARGB(255, 74, 161, 79),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
